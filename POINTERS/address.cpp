7aad7212798c45edc4eaae9ff24b7e14992c04d5
Sourabh Shiroti <sharmasaurav7372@gmail.com>
2025-08-07 18:53:56 +0530

    Add files via upload

---
+++ POINTERS/address.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    int a,b;
+    cout<<"Enter the first number :";
+    cin>>a;
+    cout<<"Enter the second number :";
+    cin>>b;
+    cout<<"Address of first number :"<<&a<<endl;
+    cout<<"Address of second number :"<<&b<<endl;
+    return 0; 
+}
